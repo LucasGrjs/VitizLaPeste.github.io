33f598a8fde62e28441b91bce0554f88373c0fa4
Lucas Grosjean <55246822+VitizLaPeste@users.noreply.github.com>
2020-11-24 10:32:08 +0100

    s

--- app.js
+++ app.js
@@ -8,57 +8,11 @@
 document.addEventListener("DOMContentLoaded", function (_e) {
 
     if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-            navigator.serviceWorker.register('/app.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-            });
-        });
-    }
-    
-
-    var cacheName = "ginko-v1";
-    var contentToCache = [];
-    var icons=["32","64","96","128","168","180","192","256","512"];
-
-    for(var i=0; i<icons.length; i++) {
-        contentToCache.push('/icons/icon-'+icons[i]+'.png');
+        if('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./sw.js');
+        };
     }
 
-    contentToCache.push('index.html');
-    contentToCache.push('app.js');
-
-    self.addEventListener('install', (e) => {
-        console.log('[Service Worker] Install');
-        e.waitUntil(
-            caches.open(cacheName).then((cache) => {
-                console.log('[Service Worker] Caching all: app shell and content');
-            return cache.addAll(contentToCache);
-            })
-        );
-    });
-
-
-    self.addEventListener('fetch', function (event) {
-        event.respondWith(
-        caches.open('ginko-dynamic').then(function (cache) {
-            return cache.match(event.request).then(function (response) {
-            return (
-                response ||
-                fetch(event.request).then(function (response) {
-                cache.put(event.request, response.clone());
-                return response;
-                })
-            );
-            });
-        }),
-        );
-  });
-
-    
     /******************************************************************
             Fonctions à compléter dans la dernière partie du TP 
     ******************************************************************/
